a7ca15324949c1010ce97eea6dd87d3f22bc5928
Matt Valdez <matvaldez@gmail.com>
2017-09-15 14:55:03 -0400

    Make it better

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
   $('.wat').on('mouseenter', function () {
     $(this).css( "background", "white" );
+    $('body').css( "background", "#2ecc71" );
     $('.links').show();
   })
   $('.wat').on('mouseleave', function () {
     $(this).css( "background", "url('img/wat_mv.png') no-repeat center center fixed" );
+    $('body').css( "background", "white" );
     $('.links').hide();
   })
 })
