6f57860079e584d8376fa36456537406a9403b1f
Matt Valdez <matvaldez@gmail.com>
2017-11-02 00:58:04 -0400

    Lots of changes, version 2.0

--- js/main.js
+++ js/main.js
@@ -1,13 +1,9 @@
 $(document).ready(function(){
-  $('.links').hide();
-  $('.wat').on('mouseenter', function () {
-    $(this).css( "background", "white" );
-    $('body').css( "background", "#2ecc71" );
-    $('.links').show();
+  $('.resume').hide();
+  $('.card').on('mouseenter', function () {
+    $('.resume').show();
   })
-  $('.wat').on('mouseleave', function () {
-    $(this).css( "background", "url('img/wat_mv.png') no-repeat center center fixed" );
-    $('body').css( "background", "#ecf0f1" );
-    $('.links').hide();
+  $('.card').on('mouseleave', function () {
+    $('.resume').hide();
   })
 })
