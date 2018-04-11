9d3ee6d35df9d37c3f39b6f04cf967b48d87f0d3
Matt Valdez <matvaldez@gmail.com>
2018-04-10 21:34:36 -0400

    Render React Card component with Contact child component

---
+++ js/components/card.js
@@ -0,0 +1,20 @@
+//
+// class Survey extends React.Component {
+//   constructor(props){
+//     super(props);
+//     this.state = {
+//       form: "hello"
+//     }
+//   }
+//   render () {
+//     return (
+//       <div>
+//         <h1>Title: {this.state.form}</h1>
+//       </div>
+//     );
+//   }
+// }
+// ReactDOM.render(
+//   <h1>Hello, world!</h1>,
+//   document.getElementById('root')
+// );
